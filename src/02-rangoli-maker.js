433ba5351f479e27fad432fe9b20e4ee82a6bc96
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2026-02-18 13:53:36 +0530

    reangoli maker is solve

--- src/02-rangoli-maker.js
+++ src/02-rangoli-maker.js
@@ -47,20 +47,61 @@
  */
 export function repeatPattern(pattern, times) {
   // Your code here
+
+  if (
+    typeof pattern !== "string" ||
+    times < 0 ||
+    Number.isInteger(times) === false
+  ) {
+    return "";
+  }
+  let data = pattern.repeat(times);
+  return data;
 }
 
 export function extractRangoliCenter(design, start, end) {
   // Your code here
+  if (
+    typeof design !== "string" ||
+    typeof start !== "number" ||
+    typeof end !== "number"
+  ) {
+    return "";
+  }
+
+  let data = design.slice(start, end);
+  return data;
 }
 
 export function splitAndJoinRangoli(colorString, oldSep, newSep) {
   // Your code here
+  if (typeof colorString !== "string") {
+    return "";
+  }
+  let data = colorString.split(oldSep);
+  let joindata = data.join(newSep);
+  return joindata;
 }
 
 export function replaceRangoliColor(design, oldColor, newColor) {
   // Your code here
+  if (
+    typeof design !== "string" ||
+    typeof oldColor !== "string" ||
+    typeof newColor !== "string"
+  ) {
+    return "";
+  }
+  let newDesign = design.replaceAll(oldColor, newColor);
+  return newDesign;
 }
 
 export function makeRangoliBorder(char, length) {
   // Your code here
+  if (typeof char !== "string" || length < 0) {
+    return "";
+  }
+  let border = char.repeat(length);
+  let newBorder = border.slice(0, length);
+  return newBorder;
 }
